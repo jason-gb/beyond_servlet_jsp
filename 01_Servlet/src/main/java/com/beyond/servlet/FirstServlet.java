d9875484dd18011c135f7f03a541eca0b6eb0f17
jason-gb <a01071865081@gmail.com>
2024-07-02 09:26:56 +0900

    수업 시작 1교시

---
+++ 01_Servlet/src/main/java/com/beyond/servlet/FirstServlet.java
@@ -0,0 +1,22 @@
+package com.beyond.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/*
+ * 서블릿을 실행시키는 방법
+ *   1. jakarta.servlet.http.HttpServlet 상속하는 클래스를 생성한다.
+ *   2. 서블릿 등록 및 매핑한다.
+ *     - web.xml에 서블릿을 등록 및 매핑한다.
+ *     - @WebServlet 어노테이션으로 서블릿을 등록 및 매핑한다.
+ */
+public class FirstServlet {
+	
+		
+}
